98fa090557922cd337f3de30fe49fac094bed237
ealene001 <estifanos.alene@gmail.com>
2020-07-28 08:55:51 -0500

    Verify 2 clients functionality on server

--- network.py
+++ network.py
@@ -6,8 +6,10 @@ class Network:
         self.server = "192.168.1.3"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.id = self.connect()
-        print(self.id)
+        self.pos = self.connect()
+
+    def getPos(self):
+        return self.pos
 
     def connect(self):
         try:
@@ -22,6 +24,3 @@ class Network:
             return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
-n = Network()
-print(n.send("Hello"))
-print(n.send("Working"))
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import sys
 server = "192.168.1.3"
 port = 5555
 
-# Initializng Socket
+# Initializing Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Binding Server and Port to Socket
@@ -19,9 +19,18 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+pos = [(0,0), (100,100)]
 # conn = connection
-def threaded_client(conn):
-    conn.send(str.encode("Connected"))
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     # Continue running while client is still connected
     while True:
@@ -29,10 +38,12 @@ def threaded_client(conn):
             # 2048 stands for how many bits of information to receive from connection
             # If error occurs when running, just increase the 2048 number
             # The larger the size is, the longer it takes to receive info
-            data = conn.recv(2048)
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
+
 
             # Decode information
-            reply = data.decode("utf-8")
+            # reply = data.decode("utf-8")
 
             # If, cannot get info from client disconnect and break
             # else, print
@@ -40,20 +51,28 @@ def threaded_client(conn):
                 print("Disconnected")
                 break
             else:
-                print("Received: ", reply)
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Received: ", data)
                 print("Sending: ", reply)
 
             # Encode string reply into byte object
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
         except:
             break
 
     print("Lost connection")
     conn.close()
+
+currentPlayer = 0
 # Continuously looking for connections
 while True:
     # String connection and address(IP)
     conn, addr = s.accept()
     print("Connected to: ", addr)
 
-    start_new_thread(threaded_client, (conn,))
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
